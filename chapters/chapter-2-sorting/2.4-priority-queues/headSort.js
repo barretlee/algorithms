fec2e2147a81563cc11656e05750a107cda36ea7
阎王 <yanwang.lj@alibaba-inc.com>
2016-05-27 17:08:41 +0800

    head sort

---
+++ chapters/chapter-2-sorting/2.4-priority-queues/headSort.js
@@ -0,0 +1,45 @@
+console.log('headSort:')
+/* input start */
+var Generator = require('../../../generator/index');
+var Checker = require('../../../generator/checker');
+var input = Generator.getRandomNumbers(20, 0, 1E2);
+input = Generator.noRepeat(input);
+input.unshift('');
+/* input end */
+console.log('> input:  ' + input);
+
+
+// headSort
+function headSort(input) {
+  return sort(input);
+
+  function sort (arr){
+    var N = arr.length - 1;
+    for(var k = N >> 2; k >= 1; k--) {
+      arr = sink(arr, k, N);
+    }
+    while(N > 1) {
+      arr[1] = [arr[N], arr[N] = arr[1]][0];
+      N--;
+      arr = sink(arr, 1, N);
+    }
+    return arr;
+  }
+  function sink(arr, k, N) {
+    while(2 * k <= N) {
+      var j = 2 * k;
+      if(j < N && arr[j] < arr[j + 1]) j++;
+      if(arr[k] >= arr[j]) break;
+      arr[k] = [arr[j], arr[j] = arr[k]][0];
+      k = j;
+    }
+    return arr;
+  }
+}
+
+
+/* output start */
+var output = headSort(input);
+console.log('> output: ' + output);
+// console.log('> binary head check: ' + Checker.binaryHeadChecker(output));
+/* output end */
--- generator/checker.js
+++ generator/checker.js
@@ -12,11 +12,12 @@ module.exports = {
    */
   binaryHeadChecker: function(output) {
     var k = 1, N = output.length - 1;
-    while(k < N) {
+    while(2 * k < N) {
       var a = 2 * k;
       var b = a + 1;
-      if(Math.max(output[a], output[b]) > output[k]) return 'not pass';
-      k = k * 2;
+      var pass = output[a] !== undefined && output[b] !== undefined;
+      if(!pass || Math.max(output[a], output[b]) > output[k]) return 'not pass';
+      k = a;
     }
     return 'pass';
   }
