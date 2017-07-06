ba218b0dd200f013195656f1014d42a3573d0984
阎王 <yanwang.lj@alibaba-inc.com>
2017-07-06 14:16:40 +0800

    binary search

--- chapters/chapter-3-searching/3.1-elementary-symbol-tables/sequentialSearchST.js
+++ chapters/chapter-3-searching/3.1-elementary-symbol-tables/sequentialSearchST.js
@@ -1,12 +1,12 @@
-console.log('sequentialSearchST:')
+// console.log('sequentialSearchST:')
 /* input start */
-var input = require('../../../generator/index').getRandomNumbers();
+// var input = require('../../../generator/index').getRandomNumbers();
 /* input end */
-console.log('> input: ' + input);
+// console.log('> input: ' + input);
 
 
 // sequentialSearchST
-function sequentialSearchST(input) {
+function sequentialSearchST() {
 
   function Node(key, val, next) {
     this.key = key;
@@ -14,7 +14,7 @@ function sequentialSearchST(input) {
   }
 
   return {
-    this.links: null,
+    links: null,
     insertWhere: function(node, where) {
       var n = new Node(node.key, node.val);
       var l = this.links;
@@ -35,21 +35,52 @@ function sequentialSearchST(input) {
     },
     findWhere: function(where) {
       var l = this.links;
-      var index = 0;
+      var depth = 0;
       while(l) {
         if(l.key == where.key || l.val == where.val) {
-          l.index = index;
-          return l;
+          var output = { depth: depth };
+          for(var key  in l) if(key !== 'next') output[key] = l[key];
+          return output;
         }
-        index++;
+        depth++;
         l = l.next;
       }
+      return -1;
     }
   }
 }
 
 
+var st = new sequentialSearchST();
+
+// insert {key: b, val: b}
+st.insertWhere({
+  key: 'b',
+  val: 'b'
+});
+// insert {key: a, val: a}
+st.insertWhere({
+  key: 'a',
+  val: 'a'
+});
+// insert {key: r, val: r} before where key = a
+st.insertWhere({
+  key: 'r',
+  val: 'r'
+}, {
+  key: 'a'
+});
+// insert {key: b, val: b}
+st.insertWhere({
+  key: 't',
+  val: 't'
+});
+
+console.log(JSON.stringify(st.links, null, 2));
+
+console.log(st.findWhere({key: 'b'}));
+
 /* output start */
-console.log('> output: ' + sequentialSearchST(input));
+// console.log('> output: ' + sequentialSearchST(input));
 /* output end */
 
