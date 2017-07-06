d2c9aee5ca145e03fed2a60b8338bc2828d21279
阎王 <yanwang.lj@alibaba-inc.com>
2017-07-06 15:48:08 +0800

    binary search tree

--- chapters/chapter-3-searching/3.2-binary-search-trees/binarySearchTree.js
+++ chapters/chapter-3-searching/3.2-binary-search-trees/binarySearchTree.js
@@ -7,13 +7,44 @@ console.log('> input: ' + input);
 
 // binarySearchTree
 function binarySearchTree(input) {
-  var bst;
+  var root;
   function Node (key, val, N) {
     this.key = key;
     this.val = val;
     this.N = N;
     this.left = null;
-    this.righ = null;
+    this.right = null;
+  }
+  function size(x) {
+    if (!x) return 0;
+    return x.N;
+  }
+  function min(x) {
+    if (!x.left) return x;
+    return min(x.left);
+  }
+  return {
+    size: function() {
+      return size(root);
+    },
+    min: function(x) {
+      return min(x).key;
+    },
+    get: function (x, key) {
+      if (!x) return null;
+      if (x.key === key) return x.val;
+      if (x.key > key) return get(x.right, key);
+      if (x.key < key) return get(x.right, key);
+    },
+    put: function(x, key, val) {
+      if (!x) return new x(key, val, 1);
+      if (x.key === key) x.val = val;
+      else if (x.key > key) put(x.right, key, val);
+      else if (x.key < key) put(x.left, key, val);
+      x.N = size(x.left) + size(x.right) + 1;
+      return x;
+    }
+    
   }
   return output;
 }
